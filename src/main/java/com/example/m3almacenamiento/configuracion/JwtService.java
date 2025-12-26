7f68d930c994272f3d4c9a075a4c4b03f29a9eaf
Octavio Noval <163550043+OctavioDNoval@users.noreply.github.com>
2025-12-26 01:24:44 -0300

    Add JWT support with token generation and validation
    Added the jjwt-api dependency to pom.xml and implemented JwtService with methods for generating JWT tokens, extracting claims, roles, and email, and validating tokens. This enables secure authentication and authorization using JWT in the application.

--- src/main/java/com/example/m3almacenamiento/configuracion/JwtService.java
+++ src/main/java/com/example/m3almacenamiento/configuracion/JwtService.java
@@ -1,7 +1,97 @@
 package com.example.m3almacenamiento.configuracion;
 
+import com.example.m3almacenamiento.modelo.enumerados.ROL;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.transaction.Transactional;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
+@Transactional
+
 public class JwtService {
+    @Value("${jwt.secret}")
+    private String key;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+
+    private SecretKey getSecretKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(key);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        String role = userDetails
+                .getAuthorities()
+                .stream()
+                .findFirst()
+                .map(GrantedAuthority::getAuthority)
+                .orElse(ROL.USER.name());
+
+        claims.put("role", role);
+        return Jwts.builder()
+                .claims(claims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis()+expiration))
+                .signWith(getSecretKey(), Jwts.SIG.HS256)
+                .compact();
+    }
+
+    public Claims excractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSecretKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String extractEmail(String token) {
+        try{
+            return excractAllClaims(token).getSubject();
+        }catch (JwtException | IllegalArgumentException e) {
+            return null;
+        }
+    }
+
+    public String extractRole(String token) {
+        try{
+            Claims claims = excractAllClaims(token);
+            String role = claims.get("role", String.class);
+            return role.startsWith("ROLE_") ? role : "ROLE_" + role;
+        }catch (JwtException | IllegalArgumentException e) {
+            return "ROLE_USER";
+        }
+    }
+
+    private Date extractExpiration(String token) {
+        try{
+            return excractAllClaims(token).getExpiration();
+        }catch (JwtException | IllegalArgumentException e) {
+            return null;
+        }
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return excractAllClaims(token).getExpiration().before(new Date());
+    }
+
+    public Boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractEmail(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
 }
